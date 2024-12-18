5523318cc029452637da86d21174ce7d966f83d3
BugHunter-X <askzenai@gmail.com>
2024-12-18 09:44:10 +0100

    Add tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+```javascript
+// error in the tailwindcss configuration file
+module.exports = {
+  // ...other configurations
+  purge: [], // This will cause tailwindcss to remove all unused styles
+  // ...rest of the configuration
+}
+```
